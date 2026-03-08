b6820f1ec2e85e3f9d843c1073dd281f31f1e6b7
Fabiano Ferreira <mff2000@gmail.com>
2026-03-08 14:36:19 -0300

    SQUAD: corrige scroll do catalogo no Sapiens

--- app32/tests/test_sapiens_workflow_catalog_ui.py
+++ app32/tests/test_sapiens_workflow_catalog_ui.py
@@ -7,3 +7,5 @@ def test_sapiens_template_contains_workflow_catalog_panel():
     assert 'workflowCatalogPanel' in template
     assert 'Catálogo Operacional de Fluxos' in template
     assert 'opsViewCatalog' in template
+    assert 'panel-mode' in template
+    assert 'sapiensChatArea' in template
