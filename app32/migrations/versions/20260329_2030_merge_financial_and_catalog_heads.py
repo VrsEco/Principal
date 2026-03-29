cfec0eea29ba957ac4540b412c25af8bfefd08a8
Fabiano Ferreira <mff2000@gmail.com>
2026-03-29 20:11:21 -0300

    Resolve multiple Alembic heads with merge revision

---
+++ app32/migrations/versions/20260329_2030_merge_financial_and_catalog_heads.py
@@ -0,0 +1,20 @@
+"""merge financial and catalog heads
+
+Revision ID: 20260329_2030
+Revises: 20260328_2300, 20260329_1400
+Create Date: 2026-03-29 20:30:00
+"""
+
+
+revision = "20260329_2030"
+down_revision = ("20260328_2300", "20260329_1400")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
