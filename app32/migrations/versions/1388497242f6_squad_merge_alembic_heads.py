61840061903de9f9984c3e9b29063829195e8b55
Fabiano Ferreira <mff2000@gmail.com>
2026-03-31 19:01:51 -0300

    SQUAD: Merge alembic heads for start_time deployment

---
+++ app32/migrations/versions/1388497242f6_squad_merge_alembic_heads.py
@@ -0,0 +1,24 @@
+"""SQUAD: Merge alembic heads
+
+Revision ID: 1388497242f6
+Revises: cb3b235ce1f1
+Create Date: 2026-03-31 19:00:47.609932
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1388497242f6'
+down_revision = 'cb3b235ce1f1'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
